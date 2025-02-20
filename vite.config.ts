3de45c7c4ff65223faa5d665648f5a06337781b8
Ali Rahbar <alirahbar2005@gmail.com>
2025-02-20 18:25:28 -0500

    added deploy stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/studylink/', // Set this to your repository name
   plugins: [
     tailwindcss(),
     react()
